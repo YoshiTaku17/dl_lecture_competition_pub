a1ece297b9f7cbae2d73aa0acafe684924b32a58
YoshiTaku17 <147139184+YoshiTaku17@users.noreply.github.com>
2024-07-17 18:09:16 +0900

    Update datasets.py

--- src/datasets.py
+++ src/datasets.py
@@ -1,38 +1,29 @@
 import os
 import numpy as np
 import torch
-from typing import Tuple
-from termcolor import cprint
+from torch.utils.data import Dataset
 
-
-class ThingsMEGDataset(torch.utils.data.Dataset):
-    def __init__(self, split: str, data_dir: str = "data") -> None:
-        super().__init__()
-        
-        assert split in ["train", "val", "test"], f"Invalid split: {split}"
+class ThingsMEGDataset(Dataset):
+    def __init__(self, split: str, data_dir: str, args) -> None:
         self.split = split
-        self.num_classes = 1854
-        
+        self.data_dir = data_dir
         self.X = torch.load(os.path.join(data_dir, f"{split}_X.pt"))
-        self.subject_idxs = torch.load(os.path.join(data_dir, f"{split}_subject_idxs.pt"))
-        
-        if split in ["train", "val"]:
-            self.y = torch.load(os.path.join(data_dir, f"{split}_y.pt"))
-            assert len(torch.unique(self.y)) == self.num_classes, "Number of classes do not match."
+        self.y = torch.load(os.path.join(data_dir, f"{split}_y.pt"))
+
+        # スケーリング
+        self.X = (self.X - self.X.mean(dim=0)) / self.X.std(dim=0)
+
+        # ベースライン補正
+        baseline = self.X[:, :, :100].mean(dim=2, keepdim=True)
+        self.X = self.X - baseline
+
+        self.num_classes = len(torch.unique(self.y))
+        self.seq_len = self.X.shape[2]
+        self.num_channels = self.X.shape[1]
 
     def __len__(self) -> int:
-        return len(self.X)
+        return len(self.y)
 
-    def __getitem__(self, i):
-        if hasattr(self, "y"):
-            return self.X[i], self.y[i], self.subject_idxs[i]
-        else:
-            return self.X[i], self.subject_idxs[i]
-        
-    @property
-    def num_channels(self) -> int:
-        return self.X.shape[1]
-    
-    @property
-    def seq_len(self) -> int:
-        return self.X.shape[2]
+    def __getitem__(self, idx: int):
+        return self.X[idx], self.y[idx]
+
